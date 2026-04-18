133d42cfd4310b5d49ebdb1dc607cc4528d4d93a
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2026-04-18 22:17:17 +0530

    Delete lovable_clone directory

--- lovable_clone/lovable_clone/src/main/java/com/codingshuttle/projects/lovable_clone/entity/ProjectMemberId.java
+++
@@ -1,7 +0,0 @@
-package com.codingshuttle.projects.lovable_clone.entity;
-
-public class ProjectMemberId
-{
-    Long projectId; // Part of PK: Identifies which project this membership belongs to
-    Long userId;    // Part of PK: Identifies which user is being granted access
-}
