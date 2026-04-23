cf25ef2e0781100baa03138d4eae6feaea6a9f4f
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2026-04-23 20:39:41 +0530

    Adding Auth APIs

--- lovable-clone/src/main/java/com/codingshuttle/projects/lovable_clone/controller/AuthController.java
+++ lovable-clone/src/main/java/com/codingshuttle/projects/lovable_clone/controller/AuthController.java
@@ -1,6 +1,25 @@
 package com.codingshuttle.projects.lovable_clone.controller;
 
+import com.codingshuttle.projects.lovable_clone.dto.auth.AuthResponse;
+import com.codingshuttle.projects.lovable_clone.dto.auth.SignupRequest;
+import com.codingshuttle.projects.lovable_clone.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
 public class AuthController
 {
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<AuthResponse> signup(SignupRequest request)
+    {
+        return ResponseEntity.ok(authService.signup(request));
+    }
 
 }
--- lovable-clone/src/main/java/com/codingshuttle/projects/lovable_clone/service/AuthService.java
+++ lovable-clone/src/main/java/com/codingshuttle/projects/lovable_clone/service/AuthService.java
@@ -1,6 +1,11 @@
 package com.codingshuttle.projects.lovable_clone.service;
 
+import com.codingshuttle.projects.lovable_clone.dto.auth.AuthResponse;
+import com.codingshuttle.projects.lovable_clone.dto.auth.SignupRequest;
+import org.jspecify.annotations.Nullable;
+
 public interface AuthService
 {
+    AuthResponse signup(SignupRequest request);
 
 }
