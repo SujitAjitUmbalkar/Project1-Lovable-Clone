67500d6f909783c8ffc00d1de7127d0967d4a27b
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2026-04-24 13:35:38 +0530

    3. createProject , used ProjectRequest dto to fetch data from user , userId is used

--- lovable-clone/src/main/java/com/codingshuttle/projects/lovable_clone/controller/ProjectController.java
+++ lovable-clone/src/main/java/com/codingshuttle/projects/lovable_clone/controller/ProjectController.java
@@ -1,14 +1,13 @@
 package com.codingshuttle.projects.lovable_clone.controller;
 
+import com.codingshuttle.projects.lovable_clone.dto.project.ProjectRequest;
 import com.codingshuttle.projects.lovable_clone.dto.project.ProjectResponse;
 import com.codingshuttle.projects.lovable_clone.dto.project.ProjectSummuryResponse;
 import com.codingshuttle.projects.lovable_clone.service.ProjectService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,6 +30,13 @@ public class ProjectController
     {
         Long userId = 1L;
         return ResponseEntity.ok(projectService.getProjectById(id , userId));
+    }
+
+    @PostMapping
+    public ResponseEntity<ProjectResponse> createProject(@RequestBody ProjectRequest request)
+    {
+        Long userId = 1L;
+        return ResponseEntity.status(HttpStatus.CREATED).body(projectService.createProject(userId, request));
     }
 
 }
--- lovable-clone/src/main/java/com/codingshuttle/projects/lovable_clone/service/ProjectService.java
+++ lovable-clone/src/main/java/com/codingshuttle/projects/lovable_clone/service/ProjectService.java
@@ -1,7 +1,9 @@
 package com.codingshuttle.projects.lovable_clone.service;
 
+import com.codingshuttle.projects.lovable_clone.dto.project.ProjectRequest;
 import com.codingshuttle.projects.lovable_clone.dto.project.ProjectResponse;
 import com.codingshuttle.projects.lovable_clone.dto.project.ProjectSummuryResponse;
+import org.jspecify.annotations.Nullable;
 
 import java.util.List;
 
@@ -10,4 +12,6 @@ public interface ProjectService
     List<ProjectSummuryResponse> getUserProjects(Long userId);
 
     ProjectResponse getProjectById(Long id, Long userId);
+
+    ProjectResponse createProject(Long userId, ProjectRequest request);
 }
