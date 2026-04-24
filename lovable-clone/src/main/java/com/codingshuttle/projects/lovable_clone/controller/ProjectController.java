75f05ef0d219f7a70888abec94421c9149b13263
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2026-04-24 13:27:37 +0530

    2. getProjectById , find project by userId and id , userId will be provided by spring security , used ProjectResponse to return dto

--- lovable-clone/src/main/java/com/codingshuttle/projects/lovable_clone/controller/ProjectController.java
+++ lovable-clone/src/main/java/com/codingshuttle/projects/lovable_clone/controller/ProjectController.java
@@ -1,10 +1,12 @@
 package com.codingshuttle.projects.lovable_clone.controller;
 
+import com.codingshuttle.projects.lovable_clone.dto.project.ProjectResponse;
 import com.codingshuttle.projects.lovable_clone.dto.project.ProjectSummuryResponse;
 import com.codingshuttle.projects.lovable_clone.service.ProjectService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +24,13 @@ public class ProjectController
     {
         Long  userId = 1L;          // will use in spring security
         return  ResponseEntity.ok(projectService.getUserProjects(userId));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProjectResponse> getProjectById(@PathVariable Long id)
+    {
+        Long userId = 1L;
+        return ResponseEntity.ok(projectService.getProjectById(id , userId));
     }
 
 }
--- lovable-clone/src/main/java/com/codingshuttle/projects/lovable_clone/service/ProjectService.java
+++ lovable-clone/src/main/java/com/codingshuttle/projects/lovable_clone/service/ProjectService.java
@@ -1,11 +1,13 @@
 package com.codingshuttle.projects.lovable_clone.service;
 
+import com.codingshuttle.projects.lovable_clone.dto.project.ProjectResponse;
 import com.codingshuttle.projects.lovable_clone.dto.project.ProjectSummuryResponse;
-import org.jspecify.annotations.Nullable;
 
 import java.util.List;
 
 public interface ProjectService
 {
     List<ProjectSummuryResponse> getUserProjects(Long userId);
+
+    ProjectResponse getProjectById(Long id, Long userId);
 }
