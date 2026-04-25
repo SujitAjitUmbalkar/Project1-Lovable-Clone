e9f6c9a7df166f8957c6c5edc38915977f4c1256
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2026-04-25 13:03:41 +0530

    2.getMySubscription(api/me/subscription) , used SubscriptinResponse , it contains PlanResponse object , status , periodEnd , tokensUsed

--- lovable-clone/src/main/java/com/codingshuttle/projects/lovable_clone/controller/BillingController.java
+++ lovable-clone/src/main/java/com/codingshuttle/projects/lovable_clone/controller/BillingController.java
@@ -1,9 +1,11 @@
 package com.codingshuttle.projects.lovable_clone.controller;
 
 import com.codingshuttle.projects.lovable_clone.dto.subscription.PlanResponse;
+import com.codingshuttle.projects.lovable_clone.dto.subscription.SubscriptionResponse;
 import com.codingshuttle.projects.lovable_clone.service.PlanService;
 import com.codingshuttle.projects.lovable_clone.service.SubscriptionService;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,5 +25,11 @@ public class BillingController
         return ResponseEntity.ok(planService.getAllActivePlans());
     }
 
+    @GetMapping("/api/me/subscription")
+    public ResponseEntity<SubscriptionResponse> getMySubsciption()
+    {
+        Long userId = 1L;
+        return ResponseEntity.ok(subscriptionService.getCurrentSubscription(userId));
+    }
 
 }
--- lovable-clone/src/main/java/com/codingshuttle/projects/lovable_clone/service/SubscriptionService.java
+++ lovable-clone/src/main/java/com/codingshuttle/projects/lovable_clone/service/SubscriptionService.java
@@ -1,6 +1,9 @@
 package com.codingshuttle.projects.lovable_clone.service;
 
+import com.codingshuttle.projects.lovable_clone.dto.subscription.SubscriptionResponse;
+
 public interface SubscriptionService
 {
 
+    SubscriptionResponse getCurrentSubscription(Long userId);
 }
