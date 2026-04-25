e9d5ad03cacbd09a2bc851423a429d6558ea6c4e
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2026-04-25 12:48:11 +0530

    Subscription and Plan APIs...

---
+++ lovable-clone/src/main/java/com/codingshuttle/projects/lovable_clone/controller/BillingController.java
@@ -0,0 +1,26 @@
+package com.codingshuttle.projects.lovable_clone.controller;
+
+import com.codingshuttle.projects.lovable_clone.dto.subscription.PlanResponse;
+import com.codingshuttle.projects.lovable_clone.service.PlanService;
+import com.codingshuttle.projects.lovable_clone.service.SubscriptionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class BillingController
+{
+    private final PlanService planService;
+    private final SubscriptionService subscriptionService;
+
+    @GetMapping("/api/plans")
+    public ResponseEntity<List<PlanResponse>> getAllPlans()
+    {
+        return ResponseEntity.ok(planService.getAllActivePlans());
+    }
+
+}
