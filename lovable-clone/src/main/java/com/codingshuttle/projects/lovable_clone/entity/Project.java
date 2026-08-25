083e29181bcb32aafa90c3b810e461b1a4e183ef
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2026-08-25 20:19:19 +0530

    5.2 - Mappings added for Project and User , Created dummy user and project , Mapstruct used

--- lovable-clone/src/main/java/com/codingshuttle/projects/lovable_clone/entity/Project.java
+++ lovable-clone/src/main/java/com/codingshuttle/projects/lovable_clone/entity/Project.java
@@ -1,31 +1,41 @@
 package com.codingshuttle.projects.lovable_clone.entity;
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.Instant;
 
-/*
- * Entity Usage:
- * Represents a coding workspace or "app" created by a user. This is the central
- * container for all files, chat sessions, and deployments associated with a specific idea.
- */
+
 @Getter
 @Setter
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Project
 {
-    Long id; // PK: Unique identifier for the project
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
 
-    String name; // Display: The name of the project (e.g., "My TodoApp ")
+    @Column(length = 100,  nullable = false)
+    String name;
 
-    User owner; // FK: The creator and primary admin of this project
+    @ManyToOne
+    @JoinColumn(nullable = false, name = "owner_id")
+    User owner;
 
-    Boolean isPublic = false; // Visibility: Determines if the project can be viewed/cloned by other users
+    Boolean isPublic = false;
 
-    Instant createdAt; // Audit: Timestamp when the project was created
-    Instant updatedAt; // Audit: Timestamp when the project details were last modified
-    Instant deletedAt; // Audit: Soft delete timestamp; if set, project is in "Trash" but not wiped
+    @CreationTimestamp
+    Instant createdAt;
+
+    @UpdateTimestamp
+    Instant updatedAt;
+
+    Instant deletedAt; // soft delete
 }
--- lovable-clone/src/main/java/com/codingshuttle/projects/lovable_clone/service/Impl/ProjectServiceImpl.java
+++ lovable-clone/src/main/java/com/codingshuttle/projects/lovable_clone/service/Impl/ProjectServiceImpl.java
@@ -3,37 +3,68 @@ package com.codingshuttle.projects.lovable_clone.service.Impl;
 import com.codingshuttle.projects.lovable_clone.dto.project.ProjectRequest;
 import com.codingshuttle.projects.lovable_clone.dto.project.ProjectResponse;
 import com.codingshuttle.projects.lovable_clone.dto.project.ProjectSummuryResponse;
+import com.codingshuttle.projects.lovable_clone.entity.Project;
+import com.codingshuttle.projects.lovable_clone.entity.User;
+import com.codingshuttle.projects.lovable_clone.mapper.ProjectMapper;
+import com.codingshuttle.projects.lovable_clone.repository.ProjectRepository;
+import com.codingshuttle.projects.lovable_clone.repository.UserRepository;
 import com.codingshuttle.projects.lovable_clone.service.FileService;
 import com.codingshuttle.projects.lovable_clone.service.ProjectService;
+import jakarta.transaction.Transactional;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE,  makeFinal = true)
+@Transactional
 public class ProjectServiceImpl implements ProjectService
 {
+    ProjectRepository projectRepository;
+    UserRepository userRepository;
+    ProjectMapper projectMapper;
+
     @Override
-    public List<ProjectSummuryResponse> getUserProjects(Long userId) {
+    public ProjectResponse createProject(Long userId, ProjectRequest request)
+    {
+        User owner = userRepository.findById(userId).orElseThrow();
+
+        Project project = Project.builder()
+                .name(owner.getName())
+                .owner(owner)
+                .build();
+
+         project = projectRepository.save(project);
+
+         return projectMapper.toProjectResponse(project);
+
+    }
+
+    @Override
+    public List<ProjectSummuryResponse> getUserProjects(Long userId)
+    {
         return List.of();
     }
 
     @Override
-    public ProjectResponse getProjectById(Long id, Long userId) {
+    public ProjectResponse getProjectById(Long id, Long userId)
+    {
         return null;
     }
 
     @Override
-    public ProjectResponse createProject(Long userId, ProjectRequest request) {
+    public ProjectResponse updateProject(Long id, ProjectRequest request, Long userId)
+    {
         return null;
     }
 
     @Override
-    public ProjectResponse updateProject(Long id, ProjectRequest request, Long userId) {
-        return null;
-    }
-
-    @Override
-    public void softDelete(Long id, Long userId) {
+    public void softDelete(Long id, Long userId)
+    {
 
     }
 }
